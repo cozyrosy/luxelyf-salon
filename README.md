c1625d1f66bd5654a953607c4d1e4bc46d9c418d
cozyrosy <gracejacob.rose@gmail.com>
2025-01-14 22:20:34 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# luxelyf-salon
---
+++ salon_users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('portfolio/', views.portfolio, name='portfolio'),
+    path('contact/', views.contact, name='contact'),
+    path('blogs/', views.blogs, name='blogs'),
+    path('blog_detail/', views.blog_detail, name='blog_detail'),
+]
