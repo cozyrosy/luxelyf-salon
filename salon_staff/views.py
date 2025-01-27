fb653b697f348b177733a725dc49c2d973bd76ac
cozyrosy <gracejacob.rose@gmail.com>
2025-01-28 03:09:19 +0530

    Added the Add and edit feature to admin side.

--- salon_staff/views.py
+++ salon_staff/views.py
@@ -26,6 +26,8 @@ def admin_staff_profiles(request):
     staffs = Staff.objects.all()
     return render(request, 'admin_staff_templates/admin_staff_profile.html', {'staffs': staffs})
 
+
+# CRUD for service categories.
 def admin_service_categories(request):
     categories = ServiceCategory.objects.all()
 
@@ -41,6 +43,40 @@ def admin_del_service_category(request, id):
     messages.success(request, 'Service category deleted successfully.')
     return redirect('admin_service_categories')
 
+def admin_edit_service_category(request, id):
+    if request.method == 'POST':
+        category = get_object_or_404(ServiceCategory, id=id)
+        category.name = request.POST['name']
+        category.description = request.POST['description']
+        category.add_to_home = 'add_to_home' in request.POST
+        category.is_active = 'is_active' in request.POST
+        if 'image' in request.FILES:
+            category.image = request.FILES['image']
+        else:
+            category.image = category.image
+        category.save()
+        messages.success(request, 'Service category updated successfully.')
+        return redirect('admin_service_categories')
+    else:
+        category = get_object_or_404(ServiceCategory, id=id)
+        return redirect('admin_service_categories')
+
+def admin_add_service_category(request):
+    if request.method == 'POST':
+        category = ServiceCategory()
+        category.name = request.POST['name']
+        category.description = request.POST['description']
+        category.add_to_home = 'add_to_home' in request.POST
+        category.is_active = 'is_active' in request.POST
+        category.image = request.FILES['image']
+        category.save()
+        messages.success(request, 'Service category added successfully.')
+        return redirect('admin_service_categories')
+    else:
+        return redirect('admin_service_categories')
+        
+
+# CRUD for services.
 def admin_services(request):
     services = Service.objects.all()
 
@@ -56,6 +92,40 @@ def admin_del_service(request, id):
     messages.success(request, 'Service deleted successfully.')
     return redirect('admin_services')
 
+def admin_edit_service(request, id):
+    if request.method == 'POST':
+        service = get_object_or_404(Service, id=id)
+        service.name = request.POST['name']
+        service.description = request.POST['description']
+        service.add_to_home = 'add_to_home' in request.POST
+        service.is_active = 'is_active' in request.POST
+        if 'image' in request.FILES:
+            service.image = request.FILES['image']
+        else:
+            service.image = service.image
+        service.save()
+        messages.success(request, 'Service updated successfully.')
+        return redirect('admin_services')
+    else:
+        service = get_object_or_404(Service, id=id)
+        return redirect('admin_services')
+
+def admin_add_service(request):
+    if request.method == 'POST':
+        service = Service()
+        service.name = request.POST['name']
+        service.description = request.POST['description']
+        service.add_to_home = 'add_to_home' in request.POST
+        service.is_active = 'is_active' in request.POST
+        service.image = request.FILES['image']
+        service.save()
+        messages.success(request, 'Service added successfully.')
+        return redirect('admin_services')
+    else:
+        return redirect('admin_services')
+
+
+# CRUD for blogs.
 def admin_blogs(request):
     blogs = Blog.objects.all().order_by('-date')
 
@@ -71,6 +141,40 @@ def admin_del_blog(request, id):
     messages.success(request, 'Blog deleted successfully.')
     return redirect('admin_blogs')
 
+def admin_edit_blog(request, id):
+    if request.method == 'POST':
+        blog = get_object_or_404(Blog, id=id)
+        blog.title = request.POST['title']
+        blog.description = request.POST['description']
+        blog.author = request.POST['author']
+        blog.date = request.POST['date']
+        if 'image' in request.FILES:
+            blog.image = request.FILES['image']
+        else:
+            blog.image = blog.image
+        blog.save()
+        messages.success(request, 'Blog updated successfully.')
+        return redirect('admin_blogs')
+    else:
+        blog = get_object_or_404(Blog, id=id)
+        return redirect('admin_blogs')
+
+def admin_add_blog(request):
+    if request.method == 'POST':
+        blog = Blog()
+        blog.title = request.POST['title']
+        blog.description = request.POST['description']
+        blog.author = request.POST['author']
+        blog.date = request.POST['date']
+        blog.image = request.FILES['image']
+        blog.save()
+        messages.success(request, 'Blog added successfully.')
+        return redirect('admin_blogs')
+    else:
+        return redirect('admin_blogs')
+
+
+# CRUD for reviews.
 def admin_reviews(request):
     reviews = Reviews.objects.all().order_by('-created_at')
 
@@ -86,6 +190,33 @@ def admin_del_review(request, id):
     messages.success(request, 'Review deleted successfully.')
     return redirect('admin_reviews')
 
+def admin_edit_review(request, id):
+    if request.method == 'POST':
+        review = get_object_or_404(Reviews, id=id)
+        review.review = request.POST['review']
+        review.author = request.POST['author']
+        review.add_to_home = 'add_to_home' in request.POST
+        review.save()
+        messages.success(request, 'Review updated successfully.')
+        return redirect('admin_reviews')
+    else:
+        review = get_object_or_404(Reviews, id=id)
+        return redirect('admin_reviews')
+
+def admin_add_review(request):
+    if request.method == 'POST':
+        review = Reviews()
+        review.review = request.POST['review']
+        review.author = request.POST['author']
+        review.add_to_home = 'add_to_home' in request.POST
+        review.save()
+        messages.success(request, 'Review added successfully.')
+        return redirect('admin_reviews')
+    else:
+        return redirect('admin_reviews')
+
+
+# CRUD for customers.
 def admin_customers(request):
     customers = UserProfile.objects.filter(user_role='customer')
 
