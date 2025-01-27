9ee2e57d2dc362ee6e02e1af3566d932d58a29d6
cozyrosy <gracejacob.rose@gmail.com>
2025-01-27 20:49:32 +0530

    Added the admin staff pages.

--- salon_staff/urls.py
+++ salon_staff/urls.py
@@ -4,4 +4,11 @@ from . import views
 urlpatterns = [
     path('', views.admin_index, name='admin_index'),
     path('admin_all_bookings/', views.admin_all_bookings, name='admin_all_bookings'),
+    path('admin_staff_profiles/', views.admin_staff_profiles, name='admin_staff_profiles'),
+    path('admin_service_categories/', views.admin_service_categories, name='admin_service_categories'),
+    path('admin_services/', views.admin_services, name='admin_services'),
+    path('admin_blogs/', views.admin_blogs, name='admin_blogs'),
+    path('admin_customers/', views.admin_customers, name='admin_customers'),
+    path('admin_reviews/', views.admin_reviews, name='admin_reviews'),
+
 ]
--- salon_staff/views.py
+++ salon_staff/views.py
@@ -9,7 +9,7 @@ from .models import Staff, StaffAvailability, Booking
 # Create your views here.
 
 def admin_index(request):
-    return render(request, 'admin_templates/admin_index.html')
+    return render(request, 'admin_staff_templates/admin_index.html')
 
 def admin_all_bookings(request):
     all_bookings = Booking.objects.all().order_by('-created_at')
@@ -18,5 +18,24 @@ def admin_all_bookings(request):
 
 
     
-    return render(request, 'admin_templates/admin_all_bookings.html', {'all_bookings': all_bookings, 'pendings': pendings
+    return render(request, 'admin_staff_templates/admin_all_bookings.html', {'all_bookings': all_bookings, 'pendings': pendings
         , 'confirmed': confirmed})
+
+def admin_staff_profiles(request):
+    staffs = Staff.objects.all()
+    return render(request, 'admin_staff_templates/admin_staff_profile.html', {'staffs': staffs})
+
+def admin_service_categories(request):
+    return render(request, 'admin_staff_templates/admin_service_categories.html')
+
+def admin_services(request):
+    return render(request, 'admin_staff_templates/admin_services.html')
+
+def admin_blogs(request):
+    return render(request, 'admin_staff_templates/admin_blogs.html')
+
+def admin_reviews(request):
+    return render(request, 'admin_staff_templates/admin_reviews.html')
+
+def admin_customers(request):
+    return render(request, 'admin_staff_templates/admin_customers.html')
